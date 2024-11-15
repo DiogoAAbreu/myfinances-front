1d668ae0930facedfd87cb630db53630858b459a
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-11-15 07:09:37 -0300

    criado esqueleto

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from '../styles/globalStyle';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+
+    </BrowserRouter>
+  );
+}
+
+export default App;
