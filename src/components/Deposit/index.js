2bac3a56ef5bc7c47923e18c2294e911786a9a50
Diogo Alberto <diogoalberto.dev@gmail.com>
2024-12-04 06:55:45 -0300

    feat: criado tela de Deposit

--- src/components/Deposit/index.js
+++ src/components/Deposit/index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import { Header } from "../common";
+import DepositForm from "./DepositForm";
 
 export default function Deposit() {
     return (
         <DepositWrapper>
             <Header>Nova entrada</Header>
-
+            <DepositForm />
         </DepositWrapper>
     );
 }
