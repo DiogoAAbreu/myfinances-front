2a403e49fd23a115a2bdb828a072c2d55c6cd9c9
Diogo Alberto <diogoalberto.dev@gmail.com>
2025-03-08 11:20:46 -0300

    feat: finalizado dimensionamento do  componente de Transaction

--- src/components/Home/Transaction.js
+++ src/components/Home/Transaction.js
@@ -1,12 +1,12 @@
 import styled from "styled-components"
 
-export default function Transaction({ date, description, value }) {
+export default function Transaction({ date, description, value, id }) {
     return (
         <TransactionWrapper>
-            <Date>02/03M</Date>
-            <Description>Metidaaaaaaaaaaaaaaa</Description>
-            <Value>2000</Value>
-            <button>X</button>
+            <Date>02/03</Date>
+            <Description>asdasdasdsssdasdgaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Description>
+            <Value>2000,00</Value>
+            <button>x</button>
         </TransactionWrapper>
     )
 }
@@ -15,14 +15,24 @@ const TransactionWrapper = styled.div`
     height: 30px;
     display: flex;
     align-items: center;
+    max-width: 100%;
+    overflow: hidden;
     padding: 8px;
     margin-top: 10px;
     font-size: 16px;
     width: 100%;
-    background-color: purple;
+    position: relative;
     button{
         color: #C6C6C6;
         margin-left:5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-weight: bold;
+        position: absolute;
+        right: 0%;
+        background-color:#fff4e6;
+        padding: 4px;
     }
 `
 
@@ -37,5 +47,8 @@ const Description = styled.span`
 
 const Value = styled.span`
     color: #C70000;
-    margin-left: auto;
+    position: absolute;
+    right: 5%;
+    background-color:#fff4e6;
+    padding-left:3px;
 `
